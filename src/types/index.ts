27a6ee0c547bcd62141ceae9aa1a4704d553a878
Savva Morozov <morozov.savvam04@yandex.ru>
2024-02-16 15:29:51 +0300

    first version

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,39 @@
-export interface ICard {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: string;
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
 }
 
-export interface IDeliveryForm  {
-    payment: string;
-    adress: string;
+export interface IDeliveryForm {
+	payment: string;
+	address: string;
 }
 
-export interface IOrderForm {
-    email: string;
-    phone: string;
+export interface IContactForm {
+	email: string;
+	phone: string;
+}
+
+export interface IOrder extends IDeliveryForm, IContactForm {
+	total: number;
+	items: string[];
+}
+
+export interface IOrderResult {
+	total: number;
+	id: string;
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface ICard extends IProduct {
+	index?: string;
+	buttonTitle?: string;
+}
+
+export interface IActions {
+	onClick: (event: MouseEvent) => void;
 }
